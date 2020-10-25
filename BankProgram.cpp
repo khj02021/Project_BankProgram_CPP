85dbe3c498843a0a18d02e3d804a7561e3f282b4
Robin Hanjun Kwon <khj02021@gmail.com>
2020-10-25 23:47:52 +0900

    Implemented printMenu function

--- BankProgram.cpp
+++ BankProgram.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void printMenu();
+
+using namespace std;
+
 int main(void) {
+	printMenu();
 
 	return 0;
+}
+
+void printMenu() {
+	cout << "------- MENU -------" << endl;
+	cout << "1. Make Account" << endl;
+	cout << "2. Deposit" << endl;
+	cout << "3. Withdraw" << endl;
+	cout << "4. Show Account info" << endl;
+	cout << "5. Exit" << endl;
 }
